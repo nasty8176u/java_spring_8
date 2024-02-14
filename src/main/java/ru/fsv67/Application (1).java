3c4db8a3a29a0a7e7ce5dfbc102393a96a2199eb
nasty8176u <110005925+nasty8176u@users.noreply.github.com>
2024-02-14 21:50:14 +0900

    Add files via upload

---
+++ src/main/java/ru/fsv67/Application (1).java
@@ -0,0 +1,32 @@
+package ru.fsv67;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    /*
+    1. Создать аннотацию замера времени исполнения метода (Timer). Она может ставиться над методами или классами.
+Аннотация работает так: после исполнения метода (метода класса) с такой аннотацией, необходимо в лог записать следующее:
+className - methodName #(количество секунд выполнения)
+
+2.* Создать аннотацию RecoverException, которую можно ставить только над методами.
+<code>
+  @interface RecoverException {
+    Class<? extends RuntimeException>[] noRecoverFor() default {};
+  }
+</code>
+У аннотации должен быть параметр noRecoverFor, в котором можно перечислить другие классы исключений.
+Аннотация работает так: если во время исполнения метода был экспешн, то не прокидывать его выше и возвращать из метода значение по умолчанию (null, 0, false, ...).
+При этом, если тип исключения входит в список перечисленных в noRecoverFor исключений, то исключение НЕ прерывается и прокидывается выше.
+3.*** Параметр noRecoverFor должен учитывать иерархию исключений.
+
+Сдавать ссылкой на файл-аспект в проекте на гитхабе.
+     */
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
